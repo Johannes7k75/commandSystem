4e6a3634a605e658e83ed6df92a867855b23f837
Johannes7k75 <Jonashebenstreit1@gmail.com>
2021-11-22 19:18:52 +0100

    solved issue with cmdStart.js

--- cmdStart.js
+++ cmdStart.js
@@ -3,7 +3,7 @@ if (event.action) {
     if (GlobalVars.getInt("CHAT_CMD_TOGGLE") === 0) {
         GlobalVars.putInt("CHAT_CMD_TOGGLE", 1);
         // set here the path to the index.js
-        let context = JsMacros.runScript("./all_Macros/commandSystem/index.js").getCtx()
+        let context = JsMacros.runScript("commandSystem/index.js").getCtx()
         GlobalVars.putObject("CHAT_CMD_CONTEXT", context)
         log('§eChatCmds §fhas been §aenabled§w!')
         // log(context.getMainThread())
